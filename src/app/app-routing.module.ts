f4ba0331efab25132a2c005433d0c8f4a5a398bf
Aleksey Knyazev <alexdefender93@gmail.com>
2020-12-27 14:06:53 +0300

    added menu, added about page placeholder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PercentCalculationComponent} from './percent-calculation/percent-calculation.component';
 import {RebalanceComponent} from "./rebalance/rebalance.component";
+import {AboutComponent} from "./static-pages/about/about.component";
 
 const routes: Routes = [
+  {path: 'about', component: AboutComponent},
   {path: 'rebalance', component: RebalanceComponent},
   {path: 'percent-calculation', component: PercentCalculationComponent},
-  {path: '**', redirectTo: 'percent-calculation'}
+  {path: '**', redirectTo: 'about'}
 ];
 
 @NgModule({
