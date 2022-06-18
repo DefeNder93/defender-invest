ef46acdab6080459c7c3a26ce16a9d6236574869
Aleksey Knyazev <alexdefender93@gmail.com>
2022-06-18 22:27:32 +0300

    first demo with 2 close lines

--- frontend/src/app/shared/components/close-chart/close-chart.component.ts
+++ frontend/src/app/shared/components/close-chart/close-chart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // TODO proper import
-declare var d3: any;
-declare var techan: any;
+declare const d3: any;
+declare const techan: any;
 
 @Component({
   selector: 'app-close-chart',
@@ -10,79 +10,83 @@ declare var techan: any;
 })
 export class CloseChartComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    const margin = {top: 20, right: 20, bottom: 30, left: 50},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
-    var parseDate = d3.timeParse("%d-%b-%y");
+    const parseDate = d3.timeParse('%d-%b-%y');
 
-    var x = techan.scale.financetime()
+    const x = techan.scale.financetime()
       .range([0, width])
       .outerPadding(0);
 
-    var y = d3.scaleLinear()
+    const y = d3.scaleLinear()
       .range([height, 0]);
 
-    var close = techan.plot.close()
+    const close = techan.plot.close()
       .xScale(x)
       .yScale(y);
 
-    var xAxis = d3.axisBottom(x);
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
 
-    var yAxis = d3.axisLeft(y);
+    const svg = d3.select('body').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    loadData('/assets/data.csv', svg, '1', 'red');
+    loadData('/assets/data2.csv', svg, '2', 'blue');
 
-    d3.csv("/assets/data.csv", function(error: any, data: any) {
-      var accessor = close.accessor();
+    function loadData(path: string, svg: any, id: string, color: string) {
+      d3.csv(path, function(error: any, data: any) {
+        const accessor = close.accessor();
 
-      data = data.slice(0, 200).map(function(d: any) {
-        return {
-          date: parseDate(d.Date),
-          open: +d.Open,
-          high: +d.High,
-          low: +d.Low,
-          close: +d.Close,
-          volume: +d.Volume
-        };
-      }).sort(function(a: any, b: any) { return d3.ascending(accessor.d(a), accessor.d(b)); });
+        data = data.slice(0, 200).map(function(d: any) {
+          return {
+            date: parseDate(d.Date),
+            open: +d.Open,
+            high: +d.High,
+            low: +d.Low,
+            close: +d.Close,
+            volume: +d.Volume
+          };
+        }).sort(function(a: any, b: any) { return d3.ascending(accessor.d(a), accessor.d(b)); });
 
-      svg.append("g")
-        .attr("class", "close");
+        svg.append('g')
+          .attr('class', 'close' + id)
+          .attr('id', 'close' + id);
 
-      svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")");
+        svg.append('g')
+          .attr('class', 'x axis')
+          .attr('transform', 'translate(0,' + height + ')');
 
-      svg.append("g")
-        .attr("class", "y axis")
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Price ($)");
+        svg.append('g')
+          .attr('class', 'y axis')
+          .append('text')
+          .attr('transform', 'rotate(-90)')
+          .attr('y', 6)
+          .attr('dy', '.71em')
+          .style('text-anchor', 'end')
+          .text('Price ($)');
+        // Only want this button to be active if the data has loaded
+        draw(data.slice(0, data.length-20), svg, id);
+        // d3.select('button').on('click', function() { draw(data, svg); }).style('display', 'inline');
 
-      // Data to display initially
-      draw(data.slice(0, data.length-20));
-      // Only want this button to be active if the data has loaded
-      d3.select("button").on("click", function() { draw(data); }).style("display", "inline");
-    });
+        const obj = document.getElementById('close' + id);
+        obj?.querySelector('path.line')?.setAttribute('style', 'stroke: ' + color);
+      });
+    }
 
-    function draw(data: any) {
+    function draw(data: any, svg: any, id: string) {
       x.domain(data.map(close.accessor().d));
       y.domain(techan.scale.plot.ohlc(data, close.accessor()).domain());
 
-      svg.selectAll("g.close").datum(data).call(close);
-      svg.selectAll("g.x.axis").call(xAxis);
-      svg.selectAll("g.y.axis").call(yAxis);
+      svg.selectAll('g.close' + id).datum(data).call(close);
+      svg.selectAll('g.x.axis').call(xAxis);
+      svg.selectAll('g.y.axis').call(yAxis);
     }
   }
 
