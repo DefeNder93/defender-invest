74a0dc44c35fb027b9f10fe29ac8b7e2c4844b8e
Aleksey Knyazev <alexdefender93@gmail.com>
2022-07-09 23:49:16 +0300

    REST API

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
+@app.route("/chart-data", methods = ['POST'])
 def hello_world():
-    with open('data/1990.json') as json_file:
+    with open('data/BCN/1990.json') as json_file:
         data = json.load(json_file)
         return {
             "username": "Alex",
