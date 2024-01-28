6825f146fe9d0ba6b48cc76d70443aed14a06813
Aleksey Knyazev <alexdefender93@gmail.com>
2024-01-28 20:20:09 +0800

    removed unused about page (#14)
    * removed unused about page
    
    * removed about page leftovers

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PercentCalculationComponent} from './percent-calculation/percent-calculation.component';
 import {RebalanceComponent} from "./rebalance/rebalance.component";
-import {AboutComponent} from "./static-pages/about/about.component";
 
 const routes: Routes = [
-  {path: 'about', component: AboutComponent},
   {path: 'rebalance', component: RebalanceComponent},
   {path: 'percent-calculation', component: PercentCalculationComponent},
   {path: 'spreads', loadChildren: () => import('./spreads/spreads.module').then((m) => m.SpreadsModule)},
-  {path: '**', redirectTo: 'about'}
+  {path: '**', redirectTo: 'rebalance'}
 ];
 
 @NgModule({
