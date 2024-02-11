6ed739c72b846a454d81dc26a754215752b4c671
Aleksey Knyazev <alexdefender93@gmail.com>
2024-02-11 22:56:50 +0800

    Issue 13: Fix Percent Calculation page (#17)

--- frontend/src/app/percent-calculation/percent-calculation.service.ts
+++ frontend/src/app/percent-calculation/percent-calculation.service.ts
@@ -5,12 +5,13 @@ import * as moment from 'moment';
   providedIn: 'root',
 })
 export class PercentCalculationService {
-  constructor() {}
-
   calculateYearlyIncome = (from: string, to: string, totalPercent: number) => {
+    if (!totalPercent) {
+      return null;
+    }
     const yearsNum =
       moment.duration(moment(to, 'YYYY-mm-dd').diff(moment(from, 'YYYY-mm-dd'))).as('days') / 365;
-
-    return totalPercent ? (Math.pow((totalPercent - 100) / 100 + 1, 1 / yearsNum) - 1) * 100 : null;
+    const result = (Math.pow(totalPercent / 100 + 1, 1 / yearsNum) - 1) * 100;
+    return Math.round(result * 100) / 100;
   };
 }
